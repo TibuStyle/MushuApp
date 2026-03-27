1f24ad6623628ba21edb17c29ad7bf8cb58c4d4e
TibuStyle <diegomau96@gmail.com>
2026-03-27 12:04:46 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWJo1KbaVG-E7az0WOLw6CCJv9fo1MK3U",
+  authDomain: "mushuapp.firebaseapp.com",
+  databaseURL: "https://mushuapp-default-rtdb.firebaseio.com",
+  projectId: "mushuapp",
+  storageBucket: "mushuapp.firebasestorage.app",
+  messagingSenderId: "781210345910",
+  appId: "1:781210345910:web:1eda2c95d9b5228571e117"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
